aa7c4c38e4e73fb2c260bfaca799d289e0e22aa1
Veijo Arponen <veijo.arponen@arpotechno.fi>
2020-07-26 14:17:33 +0300

    Add MmiTx copied from PttGpio

---
+++ src/svxlink/contrib/module_mmi/MmiTx.cpp
@@ -0,0 +1,195 @@
+/**
+@file	 MmiTx.cpp
+@brief   Multi-Mode Interface TX PTT hardware controller using GPIO
+@author  Veijo Arponen / OH4VA
+@date	 2020-07-26
+
+\verbatim
+SvxLink - A Multi Purpose Voice Services System for Ham Radio Use
+Copyright (C) 2003-2014 Tobias Blomberg / SM0SVX
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in hthe hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+\endverbatim
+*/
+
+
+
+/****************************************************************************
+ *
+ * System Includes
+ *
+ ****************************************************************************/
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <sstream>
+
+
+/****************************************************************************
+ *
+ * Project Includes
+ *
+ ****************************************************************************/
+
+
+
+/****************************************************************************
+ *
+ * Local Includes
+ *
+ ****************************************************************************/
+
+#include "MmiTx.h"
+
+
+
+/****************************************************************************
+ *
+ * Namespaces to use
+ *
+ ****************************************************************************/
+
+using namespace std;
+using namespace Async;
+
+
+
+/****************************************************************************
+ *
+ * Defines & typedefs
+ *
+ ****************************************************************************/
+
+
+
+/****************************************************************************
+ *
+ * Local class definitions
+ *
+ ****************************************************************************/
+
+
+
+/****************************************************************************
+ *
+ * Prototypes
+ *
+ ****************************************************************************/
+
+
+
+/****************************************************************************
+ *
+ * Exported Global Variables
+ *
+ ****************************************************************************/
+
+
+
+/****************************************************************************
+ *
+ * Local Global Variables
+ *
+ ****************************************************************************/
+
+
+
+/****************************************************************************
+ *
+ * Public member functions
+ *
+ ****************************************************************************/
+
+MmiTx::MmiTx(void)
+  : gpio_path("/sys/class/gpio"), active_low(false)
+{
+} /* MmiTx::MmiTx */
+
+
+MmiTx::~MmiTx(void)
+{
+} /* MmiTx::~MmiTx */
+
+
+bool MmiTx::initialize(Async::Config &cfg, const std::string name)
+{
+  cfg.getValue(name, "GPIO_PATH", gpio_path);
+
+  if (!cfg.getValue(name, "MMI_TX_PTT_PIN", gpio_pin) || gpio_pin.empty())
+  {
+    cerr << "*** ERROR: Config variable " << name << "/MMI_TX_PTT_PIN not set\n";
+    return false;
+  }
+
+  if (gpio_pin[0] == '!')
+  {
+    active_low = true;
+    gpio_pin.erase(0, 1);
+  }
+
+  stringstream ss;
+  ss << gpio_path << "/" << gpio_pin << "/value";
+  ofstream gpioval(ss.str().c_str());
+  if (gpioval.fail())
+  {
+    cerr << "*** ERROR: Could not open GPIO " << ss.str()
+         << " for writing in transmitter " << name << ".\n";
+    return false;
+  }
+  gpioval.close();
+
+  return true;
+} /* MmiTx::initialize */
+
+
+bool MmiTx::setTxOn(bool tx_on)
+{
+  cerr << "### MmiTx::setTxOn(" << (tx_on ? "true" : "false") << ")\n";
+
+  stringstream ss;
+  ss << gpio_path << "/" << gpio_pin << "/value";
+  ofstream gpioval(ss.str().c_str());
+  if (gpioval.fail())
+  {
+    return false;
+  }
+  gpioval << (tx_on ^ active_low ? 1 : 0);
+  gpioval.close();
+
+  return true;
+} /* MmiTx::setTxOn */
+
+
+
+/****************************************************************************
+ *
+ * Protected member functions
+ *
+ ****************************************************************************/
+
+
+
+/****************************************************************************
+ *
+ * Private member functions
+ *
+ ****************************************************************************/
+
+
+
+/*
+ * This file has not been truncated
+ */
